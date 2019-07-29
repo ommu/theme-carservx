e76972eae440741397251cf8c832208b2fac24b9
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-07-29 18:16:46 +0700

    fixed footer address

--- components/FooterAddress.php
+++ components/FooterAddress.php
@@ -1,8 +1,49 @@
 <?php
 namespace themes\carservx\components;
 
+use Yii;
+use ommu\core\models\CoreZoneCity;
+use ommu\core\models\CoreZoneProvince;
+use ommu\core\models\CoreZoneCountry;
+
 class FooterAddress extends \yii\base\Widget
 {
+	public $address = '92 Princess Road, parkvenue,Greater London, NW18JR, United Kingdom';
+	public $email = 'carservxdemo@gmail.com';
+	public $phone = '(+0091) 912-3456-073';
+
+	public function getAdressStatus($address): bool
+	{
+		if($address['place'] || $address['village'] || $address['city'] || $address['province'] || $address['country'] || $address['zipcode'])
+			return true;
+
+		return false;
+	}
+
+	public function getAdressMeta($address)
+	{
+		if($address['city'])
+			$address['city'] = CoreZoneCity::getInfo($address['city'], 'city_name');
+		if($address['province'])
+			$address['province'] = CoreZoneProvince::getInfo($address['province'], 'province_name');
+		if($address['country'])
+			$address['country'] = CoreZoneCountry::getInfo($address['country'], 'country_name');
+
+		return join(', ', $address);
+	}
+
+	public function init()
+	{
+		if(!Yii::$app->isDemoTheme()) {
+			$office_address = unserialize(Yii::$app->meta->get(join('_', [Yii::$app->id, 'office_address'])));
+			$office_contact = unserialize(Yii::$app->meta->get(join('_', [Yii::$app->id, 'office_contact'])));
+
+			$this->address = $this->getAdressStatus($office_address) ? $this->getAdressMeta($office_address) : '';
+			$this->email = $office_contact['email'] ? Yii::$app->formatter->asEmail($office_contact['email']) : '';
+			$this->phone = $office_contact['phone'] ? $office_contact['phone'] : '';
+		}
+	}
+
 	public function run() {
 		return $this->render('footer_address');
 	}
--- components/views/footer_address.php
+++ components/views/footer_address.php
@@ -4,20 +4,28 @@
  * @var $this app\components\View
  */
 
+use Yii;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
 $themeAsset = \themes\carservx\assets\ThemeAsset::register($this);
 $isDemoTheme = Yii::$app->isDemoTheme() ? true : false;
+$context = $this->context;
 ?>
 
 <div class="col-lg-4 col-md-4 col-sm-4">
-	<div class="widget widget_address_outer m-b20">
-		<h4 class="widget-title">Get In Touch</h4>
+	<div class="widget widget_address_outer inline-links m-b20">
+		<h4 class="widget-title"><?php echo Yii::t('app', 'Get In Touch');?></h4>
 		<ul class="widget_address">
-			<li><i class="fa fa-map-marker text-primary"></i>92 Princess Road, parkvenue,Greater London, NW18JR, United Kingdom</li>
-			<li><i class="fa fa-envelope text-primary"></i>carservxdemo@gmail.com</li>
-			<li><i class="fa fa-phone text-primary"></i>(+0091) 912-3456-073</li>
+			<?php if($context->address) {?>
+			<li><i class="fa fa-map-marker text-primary"></i><?php echo $context->address;?></li>
+			<?php }
+			if($context->email) {?>
+			<li><i class="fa fa-envelope text-primary"></i><?php echo $context->email;?></li>
+			<?php }
+			if($context->phone) {?>
+			<li><i class="fa fa-phone text-primary"></i><?php echo $context->phone;?></li>
+			<?php }?>
 		</ul>
 	</div>
 </div>
